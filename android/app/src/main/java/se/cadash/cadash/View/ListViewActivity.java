5ca9a8272e5a899b5ca1e53ca8924b648c9c3d2f
sCarlman <carl.albertsson@gmail.com>
2015-05-16 10:37:48 +0200

    LoginView created

---
+++ android/app/src/main/java/se/cadash/cadash/View/ListViewActivity.java
@@ -0,0 +1,51 @@
+package se.cadash.cadash.View;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+
+import se.cadash.cadash.R;
+
+public class ListViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_list_view, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.action_add_new_contact:
+                //Metod för nya kontakter
+                return true;
+            case R.id.action_settings:
+                //setingsshit
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
